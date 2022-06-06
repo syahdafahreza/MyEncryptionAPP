8e4549442b500b1b66c2bc208768bfcfc231873b
Syahda Fahreza <syahdafahreza@gmail.com>
2022-06-07 00:27:48 +0700

    In Dev...

--- app/src/main/java/com/myproject/myencryptionapp/DecryptActivityIMG.kt
+++ app/src/main/java/com/myproject/myencryptionapp/DecryptActivityIMG.kt
@@ -1,11 +1,17 @@
 package com.myproject.myencryptionapp
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.database.Cursor
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.os.Handler
+import android.provider.DocumentsContract
+import android.provider.OpenableColumns
 import android.util.Base64
 import android.util.Log
 import android.widget.Button
@@ -13,6 +19,8 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toFile
+import androidx.core.net.toUri
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
@@ -20,6 +28,7 @@ import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.myproject.myencryptionapp.utils.MyEncryptionMakerIMG
 import java.io.*
+import java.net.URI
 import java.security.InvalidAlgorithmParameterException
 import java.security.InvalidKeyException
 import java.security.NoSuchAlgorithmException
@@ -33,20 +42,18 @@ import javax.crypto.spec.SecretKeySpec
 
 class DecryptActivityIMG : AppCompatActivity() {
 
-    lateinit var myDirStc: File
-
     companion object {
         private val FILE_NAME_ENC = "image_enc"
         private val FILE_NAME_DEC = "image_dec.jpg"
         private val key = "PDY80oOtPHNYz1FG7"
         private val specString = "yoe6Nd84MOZCzbbO"
+        var inputan = ""
+        var initialpickername2: String = ""
     }
     val secretKey = "tK5UTui+DPh8lIlBxya5XVsmeDCoUl6vHhdIESMB6sQ="
     val salt = "QWlGNHNhMTJTQWZ2bGhpV3U=" // base64 decode => AiF4sa12SAfvlhiWu
     val iv = "bVQzNFNhRkQ1Njc4UUFaWA==" // base64 decode => mT34SaFD5678QAZX
     val cur_buffer_size=1024
-    var isiuri21 = ""
-    var isiuri22 = ""
 
     @RequiresApi(Build.VERSION_CODES.R)
     @Suppress("DEPRECATION")
@@ -75,60 +82,145 @@ class DecryptActivityIMG : AppCompatActivity() {
             })
             .check()
         val root = Environment.getExternalStorageDirectory().toString()
-        val rootnonstring = Environment.getExternalStorageDirectory()
-        val rootnonstringAlt = Environment.getStorageDirectory()
-        myDirStc = File("$root/MyEncryptionAPP")
-        if (!myDirStc.exists()) {
-            myDirStc.mkdirs()
-        }
-        Log.d("root",root)
-        Log.d("rootnotstring",rootnonstring.toString())
-        Log.d("rootnonstringAlt",rootnonstringAlt.toString())
 
+        val buttonOpenImageFileEnc = findViewById<Button>(R.id.btnOpenIMGFileEncrypted)
         val buttonBeginDecryptIMAGE = findViewById<Button>(R.id.btnBeginDecryptIMG)
 
+        buttonOpenImageFileEnc.setOnClickListener {
+            openIMGFileEnc("$root".toUri())
+        }
         buttonBeginDecryptIMAGE.setOnClickListener {
-            val outputFileDec = File(myDirStc, FILE_NAME_DEC)
-            val encFile = File(myDirStc, FILE_NAME_ENC)
+            saveIMGFileDec("$root".toUri())
+        }
+    }
 
-            try {
-                MyEncryptionMakerIMG.decryptToFile(
-                    key,
-                    specString,
-                    FileInputStream(encFile),
-                    FileOutputStream(outputFileDec)
-                )
-                val imageViewDecryptedURI = findViewById<ImageView>(R.id.imageViewDecrypted)
-                imageViewDecryptedURI.setImageURI(Uri.fromFile(outputFileDec))
+    @Suppress("DEPRECATION")
+    fun openIMGFileEnc(pickerInitialUri: Uri) {
+        val intentOpenIMGFileEnc = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "*/*"
+
+            // Optionally, specify a URI for the file that should appear in the
+            // system file picker when it loads.
+            putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri)
+        }
+
+        startActivityForResult(intentOpenIMGFileEnc, 23)
+    }
+    @Suppress("DEPRECATION")
+    fun saveIMGFileDec(pickerInitialUri: Uri) {
+        val intentSaveIMGFileDec = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "image/*"
+
+            // Optionally, specify a URI for the file that should appear in the
+            // system file picker when it loads.
+            putExtra(Intent.EXTRA_TITLE, initialpickername2+".jpg")
+            putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri)
+        }
+        startActivityForResult(intentSaveIMGFileDec, 24)
+    }
+
+    @Suppress("DEPRECATION")
+    override fun onActivityResult(
+        requestCode: Int, resultCode: Int, resultData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, resultData)
+        if (requestCode == 23
+            && resultCode == Activity.RESULT_OK
+        ) {
+            // The result data contains a URI for the document or directory that
+            // the user selected.
+            resultData?.data?.also { uri23 ->
+                // Perform operations on the document using its URI.
+                val buttonDecryptState = findViewById<Button>(R.id.btnBeginDecryptIMG)
+                buttonDecryptState.setEnabled(true)
+                dumpImageMetaData(uri23)
+                inputan = uri23.toString()
+                Log.d("inputan", inputan)
+            }
+        }
+        else if (requestCode == 24
+                && resultCode == Activity.RESULT_OK
+            ) {
+                // The result data contains a URI for the document or directory that
+                // the user selected.
+                resultData?.data?.also { uri24 ->
+                    // Perform operations on the document using its URI.
+
+                    try {
+                        decryptIMGToFile(
+                            key,
+                            specString,
+                            inputan.toUri(),
+                            uri24,
+                        )
+                        val imageUri2: Uri = uri24
+                        val decryptedImageView = findViewById<ImageView>(R.id.imageViewDecrypted)
+                        decryptedImageView.setImageURI(imageUri2)
 //                outputFileDec.delete()
 
-                Toast.makeText(this, "Decrypted!", Toast.LENGTH_SHORT).show()
-                // Handler which will run after 2 seconds.
-                // Handler which will run after 2 seconds.
-                Handler().postDelayed(Runnable {
-                    Toast.makeText(
-                        this,
-                        "Your decrypted image stored in: \n"+outputFileDec.toString(),
-                        Toast.LENGTH_LONG
-                    ).show()
-                }, 2000)
-//                Toast.makeText(this, "Your decrypted image stored in:"+outputFileDec.toString(), Toast.LENGTH_LONG).show()
-            } catch (e: Exception) {
-                e.printStackTrace()
+                        Toast.makeText(this, "Decrypted.", Toast.LENGTH_SHORT).show()
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+
+    @SuppressLint("Range")
+    fun dumpImageMetaData(uri: Uri) {
+
+        // The query, because it only applies to a single document, returns only
+        // one row. There's no need to filter, sort, or select fields,
+        // because we want all fields for one document.
+        val cursor: Cursor? = contentResolver.query(
+            uri, null, null, null, null, null)
+
+        cursor?.use {
+            // moveToFirst() returns false if the cursor has 0 rows. Very handy for
+            // "if there's anything to look at, look at it" conditionals.
+            if (it.moveToFirst()) {
+
+                // Note it's called "Display Name". This is
+                // provider-specific, and might not necessarily be the file name.
+                val displayName: String =
+                    it.getString(it.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                Log.i("TAG", "Display Name: $displayName")
+                val str2 = displayName
+                val n2 = 4
+                val resultsbstr2 = str2.dropLast(n2)
+                initialpickername2 = resultsbstr2
+                Log.i("TAG", "Display Name Disunat: $resultsbstr2")
+
+                val sizeIndex: Int = it.getColumnIndex(OpenableColumns.SIZE)
+                // If the size is unknown, the value stored is null. But because an
+                // int can't be null, the behavior is implementation-specific,
+                // and unpredictable. So as
+                // a rule, check if it's null before assigning to an int. This will
+                // happen often: The storage API allows for remote files, whose
+                // size might not be locally known.
+                val size: String = if (!it.isNull(sizeIndex)) {
+                    // Technically the column stores an int, but cursor.getString()
+                    // will do the conversion automatically.
+                    it.getString(sizeIndex)
+                } else {
+                    "Unknown"
+                }
+                Log.i("TAG", "Size: $size")
             }
         }
     }
 
-    @Throws(
-        NoSuchAlgorithmException::class,
+    @Throws(NoSuchAlgorithmException::class,
         NoSuchPaddingException::class,
         InvalidKeyException::class,
         IOException::class,
         InvalidAlgorithmParameterException::class)
-    fun decryptIMGToFile(input: InputStream, output: OutputStream) {
-//        val contentResolver = applicationContext.contentResolver
-        var output=output
-//        var output: OutputStream? = contentResolver.openOutputStream(uri22)
+    fun decryptIMGToFile(keyStr: String, spec: String,uri23: Uri, uri24: Uri) {
+
+        var input: InputStream? = contentResolver.openInputStream(uri23)
+        var output: OutputStream? = contentResolver.openOutputStream(uri24)
+
         try {
             val ivParameterSpec = IvParameterSpec(Base64.decode(iv, Base64.DEFAULT))
 
@@ -145,10 +237,11 @@ class DecryptActivityIMG : AppCompatActivity() {
             val buffer = ByteArray(cur_buffer_size)
             Log.d("COutput", output.toString())
             var bytesRead:Int = 0
-            while(input.read(buffer).also { bytesRead = it } > 0)
-                output.write(buffer, 0, bytesRead)
+            if (input != null) {
+                while(input.read(buffer).also { bytesRead = it } > 0)
+                    output.write(buffer, 0, bytesRead)
+            }
         } finally {
-            Log.d("Output buffer",output.toString())
             Log.d("Output","output.flush()")
             if (output != null) {
                 output.flush()
--- app/src/main/java/com/myproject/myencryptionapp/EncryptActivityIMG.kt
+++ app/src/main/java/com/myproject/myencryptionapp/EncryptActivityIMG.kt
@@ -1,36 +1,27 @@
 package com.myproject.myencryptionapp
 
-import android.app.Activity
 import android.Manifest
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
-import android.content.Intent.EXTRA_TITLE
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.drawable.BitmapDrawable
+import android.database.Cursor
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.DocumentsContract
+import android.provider.OpenableColumns
 import android.util.Base64
 import android.util.Log
 import android.widget.*
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.net.toFile
 import androidx.core.net.toUri
-import androidx.core.view.drawToBitmap
 import com.karumi.dexter.Dexter
-import com.karumi.dexter.DexterBuilder
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
-import com.myproject.myencryptionapp.utils.MyEncryptionMakerIMG
 import java.io.*
 import java.security.InvalidAlgorithmParameterException
 import java.security.InvalidKeyException
@@ -46,21 +37,21 @@ import javax.crypto.spec.SecretKeySpec
 class EncryptActivityIMG : AppCompatActivity() {
 
 //    lateinit var myDir: File
-    lateinit var myDirStc: File
+    lateinit var myDirStcUri: File
 
     companion object {
         private val FILE_NAME_ENC = "image_enc"
         private val FILE_NAME_DEC = "image_dec.jpg"
         private val key = "PDY80oOtPHNYz1FG7"
         private val specString = "yoe6Nd84MOZCzbbO"
+        var inputan2Enc = ""
+        var initialpickername: String = ""
     }
 
     val secretKey = "tK5UTui+DPh8lIlBxya5XVsmeDCoUl6vHhdIESMB6sQ="
     val salt = "QWlGNHNhMTJTQWZ2bGhpV3U=" // base64 decode => AiF4sa12SAfvlhiWu
     val iv = "bVQzNFNhRkQ1Njc4UUFaWA==" // base64 decode => mT34SaFD5678QAZX
     val cur_buffer_size=1024
-    var isiuri21 = ""
-    var isiuri22 = ""
 
     @RequiresApi(Build.VERSION_CODES.R)
     @Suppress("DEPRECATION")
@@ -90,48 +81,18 @@ class EncryptActivityIMG : AppCompatActivity() {
             .check()
 
         val root = Environment.getExternalStorageDirectory().toString()
-        val rootnonstring = Environment.getExternalStorageDirectory()
-        val rootnonstringAlt = Environment.getStorageDirectory()
-        myDirStc = File("$root/MyEncryptionAPP")
-        if (!myDirStc.exists()) {
-            myDirStc.mkdirs()
-        }
-        Log.d("root",root)
-        Log.d("rootnotstring",rootnonstring.toString())
-        Log.d("rootnonstringAlt",rootnonstringAlt.toString())
         val buttonOpenIMG2Encrypt = findViewById<Button>(R.id.btnOpenImg2Encrypt)
         val buttonBeginEncryptIMG = findViewById<Button>(R.id.btnBeginEncryptIMG)
 
         buttonOpenIMG2Encrypt.setOnClickListener {
             Log.d("Info","Button Open File Clicked")
-            openIMGFileUnencrypted(rootnonstring.toUri())
+            openIMGFileUnencrypted("$root".toUri())
         }
 
         buttonBeginEncryptIMG.setOnClickListener {
             Log.d("Info","Button Encrypt IMG Clicked")
-            val my_img_view = findViewById<ImageView>(R.id.imageView2Encrypt)
-            val drawable = my_img_view.getDrawable()
-            val bitmapDrawable = drawable as BitmapDrawable
-            val bitmap = bitmapDrawable.bitmap
-            val stream = ByteArrayOutputStream()
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
-            val input = ByteArrayInputStream(stream.toByteArray())
-            val contentResolver = applicationContext.contentResolver
-            val outputFileEnc = File(myDirStc, FILE_NAME_ENC)
-            try {
-                MyEncryptionMakerIMG.encryptToFile(
-                    key,
-                    specString,
-                    input,
-                    FileOutputStream(outputFileEnc)
-                )
 
-                Toast.makeText(this, "Encrypted.", Toast.LENGTH_SHORT).show()
-
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-//            saveIMGFileEncrypted(rootnonstring.toUri())
+            saveIMGFileEncrypted("$root".toUri())
 
         }
 
@@ -149,6 +110,20 @@ class EncryptActivityIMG : AppCompatActivity() {
         startActivityForResult(intentOpenUnencrypted, 21)
     }
     @Suppress("DEPRECATION")
+    fun saveIMGFileEncrypted(pickerInitialUri: Uri) {
+        val intentSaveIMGEncrypted = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "*/*"
+
+            // Optionally, specify a URI for the file that should appear in the
+            // system file picker when it loads.
+            putExtra(Intent.EXTRA_TITLE, initialpickername+"_enc")
+            putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri)
+        }
+        startActivityForResult(intentSaveIMGEncrypted, 22)
+    }
+
+    @Suppress("DEPRECATION")
     override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
         super.onActivityResult(requestCode, resultCode, resultData)
         if (requestCode == 21
@@ -158,16 +133,11 @@ class EncryptActivityIMG : AppCompatActivity() {
             // the user selected.
             resultData?.data?.also { uri21 ->
                 // Perform operations on the document using its URI.
-                print(resultData)
-                Log.d("uri21.toString()", uri21.toString())
+                inputan2Enc = uri21.toString()
+                val buttonEncryptState = findViewById<Button>(R.id.btnBeginEncryptIMG)
+                buttonEncryptState.setEnabled(true)
+                dumpImageMetaData(uri21)
                 readImageFromUri(uri21)
-                val contentResolver = applicationContext.contentResolver
-
-                val takeFlags: Int = Intent.FLAG_GRANT_READ_URI_PERMISSION or
-                        Intent.FLAG_GRANT_WRITE_URI_PERMISSION
-                // Check for the freshest data.
-                contentResolver.takePersistableUriPermission(uri21, takeFlags)
-
             }
         }
         else if (requestCode == 22
@@ -175,32 +145,20 @@ class EncryptActivityIMG : AppCompatActivity() {
             // The result data contains a URI for the document or directory that
             // the user selected.
             resultData?.data?.also { uri22 ->
-                // Perform operations on the document using its URI.
-//                val imageUri: Uri = uri
-                isiuri22 = uri22.toString()
-                Log.d("uri22tostr",uri22.toString())
-//                val my_img_view = findViewById<ImageView>(R.id.imageView2Encrypt)
-//                val outputFileEnc = my_img_view.getDrawable()
-//                val drawable = outputFileEnc
-//                val bitmapDrawable = drawable as BitmapDrawable
-//                val bitmap = bitmapDrawable.bitmap
-//                val stream = ByteArrayOutputStream()
-//                myDir = File(uri22.toString())
-//
-//                val input = ByteArrayInputStream(stream.toByteArray())
-//                bitmap.compress(Bitmap.CompressFormat.JPEG,100, stream)
-//                val contentResolver = applicationContext.contentResolver
-//
-//                try {
-//                    var myDir2Out: OutputStream? = contentResolver.openOutputStream(uri22)
-//                    Log.d("myDir",myDir.toString())
-//                    Log.d("isiuri22",isiuri22)
-//                    encryptIMGToFile(input, FileOutputStream(), applicationContext, uri22)
-//
-//
-//                } catch (e:Exception) {
-//                    e.printStackTrace()
-//                }
+
+                try {
+                    encryptIMGToFile(
+                        key,
+                        specString,
+                        inputan2Enc.toUri(),
+                        uri22
+                    )
+
+                    Toast.makeText(this, "Encrypted.", Toast.LENGTH_SHORT).show()
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
 
             }
         }
@@ -209,7 +167,6 @@ class EncryptActivityIMG : AppCompatActivity() {
     fun readImageFromUri(uri21: Uri) {
 
         val imageUri: Uri = uri21
-        isiuri21 = imageUri.toString()
         val my_img_view = findViewById<ImageView>(R.id.imageView2Encrypt)
         my_img_view.setImageURI(imageUri)
     }
@@ -219,10 +176,10 @@ class EncryptActivityIMG : AppCompatActivity() {
     InvalidKeyException::class,
     IOException::class,
     InvalidAlgorithmParameterException::class)
-    fun encryptIMGToFile(input: InputStream,output: OutputStream) {
-//        val contentResolver = applicationContext.contentResolver
-        var output=output
-//        var output: OutputStream? = contentResolver.openOutputStream(uri22)
+    fun encryptIMGToFile(keyStr: String, spec: String,uri21: Uri, uri22: Uri) {
+
+        var input: InputStream? = contentResolver.openInputStream(uri21)
+        var output: OutputStream? = contentResolver.openOutputStream(uri22)
         try {
             val ivParameterSpec = IvParameterSpec(Base64.decode(iv, Base64.DEFAULT))
 
@@ -239,10 +196,11 @@ class EncryptActivityIMG : AppCompatActivity() {
             val buffer = ByteArray(cur_buffer_size)
             Log.d("COutput", output.toString())
             var bytesRead:Int = 0
-            while(input.read(buffer).also { bytesRead = it } > 0)
-                output.write(buffer, 0, bytesRead)
+            if (input != null) {
+                while(input.read(buffer).also { bytesRead = it } > 0)
+                    output.write(buffer, 0, bytesRead)
+            }
     } finally {
-        Log.d("Output buffer",output.toString())
         Log.d("Output","output.flush()")
             if (output != null) {
                 output.flush()
@@ -255,17 +213,48 @@ class EncryptActivityIMG : AppCompatActivity() {
 
     }
 
-    @Suppress("DEPRECATION")
-    fun saveIMGFileEncrypted(pickerInitialUri: Uri) {
-        val intentSaveIMGEncrypted = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
-            addCategory(Intent.CATEGORY_OPENABLE)
-            type = "*/*"
+    @SuppressLint("Range")
+    fun dumpImageMetaData(uri: Uri) {
 
-            // Optionally, specify a URI for the file that should appear in the
-            // system file picker when it loads.
-            putExtra(Intent.EXTRA_TITLE,"your_encrypted_image_enc")
-            putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri)
+        // The query, because it only applies to a single document, returns only
+        // one row. There's no need to filter, sort, or select fields,
+        // because we want all fields for one document.
+        val cursor: Cursor? = contentResolver.query(
+            uri, null, null, null, null, null)
+
+        cursor?.use {
+            // moveToFirst() returns false if the cursor has 0 rows. Very handy for
+            // "if there's anything to look at, look at it" conditionals.
+            if (it.moveToFirst()) {
+
+                // Note it's called "Display Name". This is
+                // provider-specific, and might not necessarily be the file name.
+                val displayName: String =
+                    it.getString(it.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                Log.i("TAG", "Display Name: $displayName")
+                val str1 = displayName
+                val n = 4
+                val resultsbstr = str1.dropLast(n)
+                initialpickername = resultsbstr
+                Log.i("TAG", "Display Name: $resultsbstr")
+
+                val sizeIndex: Int = it.getColumnIndex(OpenableColumns.SIZE)
+                // If the size is unknown, the value stored is null. But because an
+                // int can't be null, the behavior is implementation-specific,
+                // and unpredictable. So as
+                // a rule, check if it's null before assigning to an int. This will
+                // happen often: The storage API allows for remote files, whose
+                // size might not be locally known.
+                val size: String = if (!it.isNull(sizeIndex)) {
+                    // Technically the column stores an int, but cursor.getString()
+                    // will do the conversion automatically.
+                    it.getString(sizeIndex)
+                } else {
+                    "Unknown"
+                }
+                Log.i("TAG", "Size: $size")
+            }
         }
-        startActivityForResult(intentSaveIMGEncrypted, 22)
     }
+
 }
